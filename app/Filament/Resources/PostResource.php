2f5e0026bf1e5fb25bcdc8106d4d03bbaa29e287
waner1er <erwan@derniercri.io>
2022-05-10 15:12:22 +0200

    :sparkles: integrate gutenberg field to postForm

--- app/Filament/Resources/PostResource.php
+++ app/Filament/Resources/PostResource.php
@@ -4,6 +4,7 @@ namespace App\Filament\Resources;
 
 use App\Filament\Resources\PostResource\Pages;
 use App\Filament\Resources\PostResource\RelationManagers;
+use App\Forms\Components\Gutenberg;
 use App\Models\Age;
 use App\Models\Category;
 use App\Models\Post;
@@ -39,7 +40,7 @@ class PostResource extends Resource
                     'default' => 1,
                 ])
                     ->schema([
-                        RichEditor::make('body')->required(),
+                        Gutenberg::make('body'),
                     ])
             ]);
     }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use VanOns\Laraberg\Http\Controllers\OEmbedController;
+use VanOns\Laraberg\Http\Controllers\BlockController;
 use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
@@ -24,14 +26,12 @@ Route::resource('/posts', PostController::class)->middleware('auth');
 Route::post('post', [PostController::class, 'store'])->name('posts.store')->middleware('auth');
 Route::post('post-update', [PostController::class, 'update'])->name('posts.update')->middleware
 ('auth');
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
+
 
 require __DIR__.'/auth.php';
 
 
 Route::group(['prefix' => 'laraberg', 'middleware' => ['web', 'auth']], function() {
-    Route::apiResource('blocks', 'VanOns\Laraberg\Http\Controllers\BlockController');
-    Route::get('oembed', 'VanOns\Laraberg\Http\Controllers\OEmbedController');
+    Route::apiResource('blocks', BlockController::class);
+    Route::get('oembed', OEmbedController::class);
 });
