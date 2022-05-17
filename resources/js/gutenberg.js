a5f750a53c48052c4f48f195c1838104f3a21c68
waner1er <erwan@derniercri.io>
2022-05-17 10:49:09 +0200

    feat/create a new custom block

--- resources/js/gutenberg.js
+++ resources/js/gutenberg.js
@@ -7,10 +7,18 @@ Laraberg.init('content',
         laravelFilemanager: true,
 
     });
+
+let title = 'my-custom-blocks'
+let slug = 'my-custom-blocks'
+Laraberg.registerCategory(title, slug)
+
+
+
+//first test custom block
 const myBlock =  {
     title: 'My First Block!',
     icon: 'universal-access-alt',
-    category: 'my-category',
+    category: 'my-custom-blocks',
 
     edit() {
         return "<h1>Hello editor.</h1>"
@@ -21,4 +29,4 @@ const myBlock =  {
     }
 }
 
-Laraberg.registerBlock('testing_laraberg', myBlock)
+Laraberg.registerBlock('testing-laraberg/myblock', myBlock)
